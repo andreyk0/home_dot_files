1a66d73bf94b044c2c1ddb0cc7d085a31e362beb
Andrey Kartashov <andrey.kartashov@gmail.com>
2019-11-02 21:48:42 -0400

    nix env

--- default.nix
+++ default.nix
@@ -1,2 +1,12 @@
 with import <nixpkgs> {};
-in default
+
+# nix-build .dotfiles/default.nix
+# nix-env --set /nix/store/9l8kz3pvj7mnhirl8z9qhi0mr9206lfq-default
+buildEnv {
+  name = "default";
+  paths = [
+    dhall
+    kubectl
+    nix
+  ];
+}
