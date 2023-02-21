59ccf6dbc9b1a01c7e16ce62bf16bd8486f4223b
Andrey Kartashov <akartashov@earnestanalytics.com>
2023-02-21 10:52:48 -0500

    doom theme

--- doom.d/config.el
+++ doom.d/config.el
@@ -40,12 +40,13 @@
 ;; (setq doom-theme 'doom-one)
 ;;
 ;; https://github.com/hlissner/emacs-doom-themes/tree/screenshots
+(setq doom-theme 'doom-1337)
 ;; (setq doom-theme 'doom-gruvbox)
 ;; (setq doom-theme 'doom-Iosvkem)
 ;; (setq doom-theme 'doom-material)
 ;; (setq doom-theme 'doom-monokai-classic)
 ;; (setq doom-theme 'doom-monokai-pro)
-(setq doom-theme 'doom-old-hope)
+;; (setq doom-theme 'doom-old-hope)
 ;; (setq doom-theme 'doom-one)
 ;; (setq doom-theme 'doom-peacock)
 ;; (setq doom-theme 'doom-snazzy)
