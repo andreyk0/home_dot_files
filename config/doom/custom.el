77426b8ce41f44083157c465ffb5ec014cf2d11f
Andrey Kartashov <akartashov@earnestanalytics.com>
2025-02-07 16:10:45 -0500

    projectile root files

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -4,10 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(lsp-haskell-completion-in-comments nil)
-
- ;; Extra project roots
- '(project-vc-extra-root-markers
-   '(".projectile" ".project.el" "build.sbt" "requirements.txt")))
+ '(projectile-project-root-files-bottom-up
+   '(".ccls-root" ".project" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".pijul" ".sl" ".jj" ".project.el" "build.sbt" "Cargo.toml")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
