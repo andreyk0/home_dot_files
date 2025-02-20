625884aa9dad2f6919f15b7dac9640304f1ed487
Andrey Kartashov <akartashov@earnestanalytics.com>
2025-02-20 08:23:27 -0500

    theme

--- config/doom/config.el
+++ config/doom/config.el
@@ -52,6 +52,7 @@
 ;; (setq doom-theme 'doom-one)
 ;;
 ;; https://github.com/hlissner/emacs-doom-themes/tree/screenshots
+(setq doom-theme 'doom-xcode)
 ;; (setq doom-theme 'doom-bluloco-dark)
 ;; (setq doom-theme 'doom-gruvbox)
 ;; (setq doom-theme 'doom-Iosvkem)
@@ -62,7 +63,7 @@
 ;; (setq doom-theme 'doom-monokai-ristretto)
 ;; (setq doom-theme 'doom-monokai-spectrum)
 ;; (setq doom-theme 'doom-oceanic-next)
-(setq doom-theme 'doom-old-hope)
+;; (setq doom-theme 'doom-old-hope)
 ;; (setq doom-theme 'doom-one)
 ;; (setq doom-theme 'doom-opera)
 ;; (setq doom-theme 'doom-peacock)
