b36647e13c8982a92c058743be59830a66ff83ef
Andrey Kartashov <akartashov@earnestanalytics.com>
2025-05-09 17:11:28 -0400

    themes

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,27 +3,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(consult-themes '(
+                    doom-bluloco-dark
+                    doom-gruvbox
+                    doom-Iosvkem
+                    doom-material
+                    doom-material-dark
+                    doom-monokai-classic
+                    doom-monokai-pro
+                    doom-monokai-ristretto
+                    doom-monokai-spectrum
+                    doom-oceanic-next
+                    doom-old-hope
+                    doom-one
+                    doom-opera
+                    doom-peacock
+                    doom-snazzy
+                    doom-tomorrow-night
+                    doom-xcode
+                    doom-zenburn
+                    wombat
+                    ))
  '(lsp-haskell-completion-in-comments nil)
  '(projectile-project-root-files-bottom-up
-   '(
-	"build.sbt"
-	".bzr"
-	"Cargo.toml"
-	".ccls-root"
-	"_darcs"
-	"_FOSSIL_"
-	".fslckout"
-	".git"
-	".hg"
-	".jj"
-	"package.json"
-	".pijul"
-	".project"
-	".project.el"
-	"pyproject.toml"
-	".sl"
-	"stack.yaml"
-     ))
+   '("build.sbt" ".bzr" "Cargo.toml" ".ccls-root" "_darcs" "_FOSSIL_" ".fslckout"
+     ".git" ".hg" ".jj" "package.json" ".pijul" ".project" ".project.el"
+     "pyproject.toml" ".sl" "stack.yaml"))
  '(sql-formatter-dialect "postgresql"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
