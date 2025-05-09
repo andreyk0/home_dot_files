8bf144ade8148bb948745348501feb784e97ff98
Andrey Kartashov <akartashov@earnestanalytics.com>
2025-05-09 17:32:57 -0400

    themes

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -4,12 +4,31 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(consult-themes '(
+                    doom-1337
+                    doom-ayu-dark
+                    doom-ayu-mirage
+                    doom-badger
                     doom-bluloco-dark
+                    doom-challenger-deep
+                    doom-city-lights
+                    doom-dark+
+                    doom-feather-dark
                     doom-gruvbox
+                    doom-henna
+                    doom-homage-black
+                    doom-horizon
                     doom-Iosvkem
+                    doom-ir-black
+                    doom-lantern
+                    doom-laserwave
+                    doom-manegarm
                     doom-material
                     doom-material-dark
+                    doom-miramare
+                    doom-molokai
                     doom-monokai-classic
+                    doom-monokai-machine
+                    doom-monokai-octagon
                     doom-monokai-pro
                     doom-monokai-ristretto
                     doom-monokai-spectrum
@@ -17,12 +36,19 @@
                     doom-old-hope
                     doom-one
                     doom-opera
+                    doom-palenight
                     doom-peacock
+                    doom-pine
+                    doom-plain-dark
+                    doom-rouge
                     doom-snazzy
+                    doom-sourcerer
+                    doom-tokyo-night
                     doom-tomorrow-night
+                    doom-vibrant
+                    doom-wilmersdorf
                     doom-xcode
                     doom-zenburn
-                    wombat
                     ))
  '(lsp-haskell-completion-in-comments nil)
  '(projectile-project-root-files-bottom-up
