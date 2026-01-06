0c0e823f087f622202cf21f996e31465dd57ed76
Andrey Kartashov <akartashov@earnestanalytics.com>
2026-01-06 14:24:42 -0500

    workaround lsp issue
    Disable hlint hooks for now
    https://github.com/haskell/haskell-language-server/issues/4674#issuecomment-3570828809

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,54 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(consult-themes '(
-                    doom-1337
-                    doom-ayu-dark
-                    doom-ayu-mirage
-                    doom-badger
-                    doom-bluloco-dark
-                    doom-challenger-deep
-                    doom-city-lights
-                    doom-dark+
-                    ;; doom-feather-dark
-                    doom-gruvbox
-                    doom-henna
-                    doom-homage-black
-                    doom-horizon
-                    doom-Iosvkem
-                    doom-ir-black
-                    doom-lantern
-                    doom-laserwave
-                    doom-manegarm
-                    doom-material
-                    doom-material-dark
-                    doom-miramare
-                    doom-molokai
-                    doom-monokai-classic
-                    doom-monokai-machine
-                    doom-monokai-octagon
-                    doom-monokai-pro
-                    doom-monokai-ristretto
-                    doom-monokai-spectrum
-                    doom-oceanic-next
-                    doom-old-hope
-                    doom-one
-                    doom-opera
-                    doom-palenight
-                    doom-peacock
-                    doom-pine
-                    doom-plain-dark
-                    doom-rouge
-                    doom-snazzy
-                    doom-sourcerer
-                    ;; doom-tokyo-night
-                    doom-tomorrow-night
-                    doom-vibrant
-                    doom-wilmersdorf
-                    doom-xcode
-                    doom-zenburn
-                    ))
+ '(consult-themes
+   '(doom-1337 doom-ayu-dark doom-ayu-mirage doom-badger doom-bluloco-dark
+     doom-challenger-deep doom-city-lights doom-dark+ doom-gruvbox doom-henna
+     doom-homage-black doom-horizon doom-Iosvkem doom-ir-black doom-lantern
+     doom-laserwave doom-manegarm doom-material doom-material-dark doom-miramare
+     doom-molokai doom-monokai-classic doom-monokai-machine doom-monokai-octagon
+     doom-monokai-pro doom-monokai-ristretto doom-monokai-spectrum
+     doom-oceanic-next doom-old-hope doom-one doom-opera doom-palenight
+     doom-peacock doom-pine doom-plain-dark doom-rouge doom-snazzy
+     doom-sourcerer doom-tomorrow-night doom-vibrant doom-wilmersdorf doom-xcode
+     doom-zenburn))
  '(lsp-haskell-completion-in-comments nil)
+ '(lsp-haskell-plugin-hlint-code-actions-on nil)
+ '(lsp-haskell-plugin-hlint-diagnostics-on nil)
  '(projectile-project-root-files-bottom-up
    '("build.sbt" ".bzr" "Cargo.toml" ".ccls-root" "_darcs" "_FOSSIL_" ".fslckout"
      ".git" ".hg" ".jj" "package.json" ".pijul" ".project" ".project.el"
